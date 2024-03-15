137a7c734ed3a41c9f9dfdbdd07dcd92006d30e8
BlazejMrzyglod <blazej.mrzyglod@onet.pl>
2024-03-15 14:19:09 +0100

    initial commit

---
+++ lab1/lab1.R
@@ -0,0 +1,36 @@
+
+a = 2*log(5)
+b = a*3
+min(a,b)
+help(median)
+a = c(25:45)
+median(a)
+apropos("median",mode="function")
+setwd("C:/Users/blaze/Pulpit/Studia/APU/APU/lab1")
+a = "macbooki Apple"
+save(a,file="workspace.RData")
+remove(a)
+a
+load("workspace.RDataa")
+a
+install.packages("gridExtra")
+help(package="gridExtra")
+library(gridExtra)
+top10 = head(Orange, 10)
+grid.table(top10)
+wektor = seq(200,130,-5)
+a = c(19:5)
+b = c(11:23)
+d = c(b,a)
+d
+nazwy = c("Apple MacBook Air", "Apple MacBook Pro 2023", "Apple MacBook Air 2024", "Apple MacBook Air M1", "Apple MacBook Air M1", "Apple MacBook Air 2024", "Apple MacBook Air M1", "Apple MacBook Air 2024", "Apple MacBook Air", "Apple MacBook Air 2024")
+ekran = c('13,6"','14,2"','15,3"','13,3"','13,3"','13,6"','13,3"','13,6"','13,6"','13,6"')
+pamiec_RAM = c(8,18,8,8,16,8,8,8,16,8)
+dysk=c(256,512,512,256,256,256,256,256,256,256)
+cena=c(4999,10199,8199,4579,5699,5999,4999,5999,6349,5999)
+liczba_opinii=c(8,5,0,78,114,0,72,0,19,0)
+Macbooki = data.frame(nazwa = nazwy, ekran, pamiec_RAM,dysk,cena, liczba_opinii)
+mean(Macbooki$cena)
+nowy_Macbook = data.frame(nazwa="MacBook Air M1", ekran='13,3"', pamiec_RAM=8,dysk=256,cena=4579, liczba_opinii=113)
+Macbooki <- rbind(Macbooki, nowy_Macbook)
+mean(Macbooki$cena)
